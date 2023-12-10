43e27be9547c194be08f4f45d5a50d105a143f3a
bora8572 <bora8572@gmail.com>
2023-12-10 11:25:37 +0500

    Format queries text

--- sql_test_assignment/models/task3.sql
+++ sql_test_assignment/models/task3.sql
@@ -1,9 +1,9 @@
-SELECT  id,
-		value,
-		client_id,
-		client_name,
-		payment_date,
-		manager_name,
-		manager_email,
-		IF(ROW_NUMBER() OVER(PARTITION BY client_id ORDER BY payment_date, id) = 1, 'Новый', 'Действующий') client_state
+SELECT id,
+       value,
+       client_id,
+       client_name,
+       payment_date,
+       manager_name,
+       manager_email,
+       IF(ROW_NUMBER() OVER(PARTITION BY client_id ORDER BY payment_date, id) = 1, 'Новый', 'Действующий') client_state
 FROM payments
--- sql_test_assignment/models/task4.sql
+++ sql_test_assignment/models/task4.sql
@@ -1,18 +1,18 @@
-SELECT  CASE 
-		WHEN toMonth(payment_date) = 1 THEN CONCAT('Январь ', toYear(payment_date))
-		WHEN toMonth(payment_date) = 2 THEN CONCAT('Февраль ', toYear(payment_date))
-		WHEN toMonth(payment_date) = 3 THEN CONCAT('Март ', toYear(payment_date))
-		WHEN toMonth(payment_date) = 4 THEN CONCAT('Апрель ', toYear(payment_date))
-		WHEN toMonth(payment_date) = 5 THEN CONCAT('Май ', toYear(payment_date))
-		WHEN toMonth(payment_date) = 6 THEN CONCAT('Июнь ', toYear(payment_date))
-		WHEN toMonth(payment_date) = 7 THEN CONCAT('Июль ', toYear(payment_date))
-		WHEN toMonth(payment_date) = 8 THEN CONCAT('Август ', toYear(payment_date))
-		WHEN toMonth(payment_date) = 9 THEN CONCAT('Сентябрь ', toYear(payment_date))
-		WHEN toMonth(payment_date) = 10 THEN CONCAT('Октябрь ', toYear(payment_date))
-		WHEN toMonth(payment_date) = 11 THEN CONCAT('Ноябрь ', toYear(payment_date))
-		ELSE CONCAT('Декабрь ', toYear(payment_date))
-		END period,
-		SUM(value) revenue_by_month,
-		SUM(SUM(value)) OVER(ORDER BY MIN(id)) revenue_cumulative
+SELECT CASE 
+	   WHEN toMonth(payment_date) = 1 THEN CONCAT('Январь ', toYear(payment_date))
+	   WHEN toMonth(payment_date) = 2 THEN CONCAT('Февраль ', toYear(payment_date))
+	   WHEN toMonth(payment_date) = 3 THEN CONCAT('Март ', toYear(payment_date))
+	   WHEN toMonth(payment_date) = 4 THEN CONCAT('Апрель ', toYear(payment_date))
+	   WHEN toMonth(payment_date) = 5 THEN CONCAT('Май ', toYear(payment_date))
+	   WHEN toMonth(payment_date) = 6 THEN CONCAT('Июнь ', toYear(payment_date))
+	   WHEN toMonth(payment_date) = 7 THEN CONCAT('Июль ', toYear(payment_date))
+	   WHEN toMonth(payment_date) = 8 THEN CONCAT('Август ', toYear(payment_date))
+	   WHEN toMonth(payment_date) = 9 THEN CONCAT('Сентябрь ', toYear(payment_date))
+	   WHEN toMonth(payment_date) = 10 THEN CONCAT('Октябрь ', toYear(payment_date))
+	   WHEN toMonth(payment_date) = 11 THEN CONCAT('Ноябрь ', toYear(payment_date))
+	   ELSE CONCAT('Декабрь ', toYear(payment_date))
+	   END period,
+       SUM(value) revenue_by_month,
+       SUM(SUM(value)) OVER(ORDER BY MIN(id)) revenue_cumulative
 FROM payments
 GROUP BY period
